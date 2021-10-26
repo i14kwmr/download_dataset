ac0968a9030cbceedd42c82c2a58d38e17d6fdd2
i14kwmr <i14kawamura@gmail.com>
2021-10-27 08:38:22 +0900

    modified: minor correction

--- download_dataset.sh
+++ download_dataset.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
+# make folder (dataset)
+if [ ! -d dataset ]
+then
+  mkdir dataset
+fi
+cd dataset
+
+# make folder (vbd)
+if [ ! -d vbd ]
+then
+  mkdir vbd
+fi
+cd vbd
+
+# file name and url
 fname=dataset.zip
 url=https://datashare.ed.ac.uk/download/DS_10283_1942.zip
 
 # wget
 wget -O $fname $url
 
-# unzip
-unzip $fname
+# unzip (datast.zipはオプションでデータセットを指定できる)
+#unzip $fname
